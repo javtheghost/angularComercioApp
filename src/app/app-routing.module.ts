cf9d578192a9e26bd343b4ee948f1e52180ad50d
javtheghost <callofgamer34@gmail.com>
2022-05-15 19:19:54 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './views/auth/login/login.component';
+import { RegisterComponent } from './views/auth/register/register.component';
+import { HomeComponent } from './views/home/home.component';
+import { CreateComponent } from './views/products/create/create.component';
+import { EditComponent } from './views/products/edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch:'full'},
+  { path:'login', component:LoginComponent},
+  { path:'home', component:HomeComponent},
+  { path:'edit', component:EditComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponentes = [LoginComponent, HomeComponent, RegisterComponent, CreateComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponentes } from './app-routing.module';
 import { AppComponent } from './app.component';
+/*import { EditComponent } from './views/products/edit/edit.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/auth/login/login.component';
+import { RegisterComponent } from './views/auth/register/register.component';
+import { CreateComponent } from './views/products/create/create.component';*/
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponentes
   ],
   imports: [
     BrowserModule,
